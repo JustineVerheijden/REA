28e568a525dd4dca592b93896247d526b0019804
JustineVerheijden <30886064+JustineVerheijden@users.noreply.github.com>
2018-04-04 11:41:43 +1000

    Add files via upload

--- jasmineTests/spec/resultsSpec.js
+++ jasmineTests/spec/resultsSpec.js
@@ -1,16 +1,30 @@
 'use strict'
 describe('Results Controller', function(){
-    var $controller;    
+    let $rootScope, $q, dataService, $controller;
+    let resultsValue={};    
 
     beforeEach(module('REA'));
-    beforeEach(inject(function(_$controller_){
+    beforeEach(inject(function(_$rootScope_, _$q_, _dataService_, _$controller_){
         $controller = _$controller_;
+        dataService = _dataService_;
+        $q = _$q_;
+        $rootScope = _$rootScope_;
     }));
     describe('Initialise controller', function(){
-        
         it("should be created successfully", function() {
-            $controller = $controller('ResultsController');
+            $controller = $controller('ResultsController', {dataService:dataService});
             expect($controller).toBeDefined();
         });
-    })
+        
+        it("should retrieve data from data service", function(){
+            spyOn(dataService, 'getResultsProperties').and.callFake(function(){
+                var deferred = $q.defer();
+                deferred.resolve(resultsValue);
+                return deferred.promise;    
+            });
+            $controller = $controller('ResultsController', {dataService:dataService});
+            $rootScope.$apply();
+            expect(dataService.getResultsProperties).toHaveBeenCalled();
+        });
+    });
 });
