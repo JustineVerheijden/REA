e1ef800d2407e9bff47091d9a4d3a6b12b45465e
JustineVerheijden <30886064+JustineVerheijden@users.noreply.github.com>
2018-04-12 14:32:03 +1000

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,11 +1,15 @@
 import angular from 'angular';
+import dataService from './dataService';
 import ResultsController from './resultsController';
 import "../bootstrap/css/bootstrap.css";
 import '../css/selectProperties.css';
 
 var moduleName = 'REA';
 
-let app = angular.module(moduleName,[]);
-app.controller('ResultsController',[ResultsController]);
+let app = angular.module(moduleName,[dataService.name]);
+
+ResultsController.$inject = ['dataService'];
+
+app.controller('ResultsController',ResultsController);
 
 export default moduleName;
